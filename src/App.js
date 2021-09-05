c24a66f8180151526c16a08dc49d1a739662a170
mscafate <michaelscafate@yahoo.com>
2021-09-05 16:22:12 -0400

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <Header />
+      {/* Body */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBmAQPlg4d1v_wbYLnFeNIha0m_Q-HP240",
+    authDomain: "clone-3ca94.firebaseapp.com",
+    projectId: "clone-3ca94",
+    storageBucket: "clone-3ca94.appspot.com",
+    messagingSenderId: "768952343329",
+    appId: "1:768952343329:web:813a6de78f354f803d3c60",
+    measurementId: "G-SQ2CTZ625K"
+  };
